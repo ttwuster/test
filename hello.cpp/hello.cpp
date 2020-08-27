8dded3dde0f601b6296694935d40e79f36719a06
Tristab Wuster <ttwuster@mavs.coloradomesa.edu>
2020-08-27 12:10:10 -0600

    changed to radius code

---
+++ hello.cpp/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[])
+{
+    int radius = 12 ;
+    int pie = 3.1414  ;
+    int perimeter = 2*pie*radius ;
+    int area = pie*radius^2 ;
+    
+    cout << " perimeter of a circle is: "<< perimeter << endl;
+    cout << " area of a cirlce is: "<< area << endl;
+    
+
+    return 0;
+}
